33dea3884f77f37f5c5d2bbfc15d0f5e06d00dff
carmonasl <alberto.carmona.sanchez@gmail.com>
2023-02-13 18:01:01 +0100

    Github URL

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
+            href: 'https://github.com/ODIN-PROTOCOL',
             label: 'GitHub',
             position: 'right',
           },
